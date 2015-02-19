2ea9fbe66fb314d4932d6d013c427a8e029e9f0f
Michael MacKinnon <mike@eth0.ca>
2015-02-18 17:16:35 -0700

    add readme and package.json

---
+++ README.md
@@ -0,0 +1,3 @@
+npm install jsx
+
+jsx --watch src/ build/
--- src/helloworld.js
+++ src/helloworld.js
@@ -77,7 +77,16 @@ React.render(
     document.getElementById('example')
 );
 
+var CheckLink = React.createClass({
+    render: function() {
+        // This takes any props passed to CheckLink and copies them to <a>
+        return <a {...this.props}>{'√ '} {this.props.children}</a>;
+    }
+});
+
 React.render(
-    <Avatar username="mike" />,
+    <CheckLink href="/checked.html">
+        Click here!
+    </CheckLink>,
     document.getElementById('example2')
 );
